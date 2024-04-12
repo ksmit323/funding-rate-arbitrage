45709fa9b4f2fa119f98e9c84c31850c05ff6640
Kenneth Smith <ksmit323@gmail.com>
2024-04-12 10:19:17 +0700

    add functions, update create_order

--- src/order.py
+++ src/order.py
@@ -17,7 +17,7 @@ class OrderType(StrEnum):
 
 
 class Order(object):
-    def __init__ (
+    def __init__(
         self,
         config: Config,
         session: Session,
@@ -29,33 +29,41 @@ class Order(object):
         self._signer = signer
         self._account = account
 
-    def get_orders(self):
-        req = self._signer.sign_request(
-            Request("GET", "%s/v1/orders" % self._config.base_url)
-        )
+    def _send_request(self, request: Request):
+        """Helper function"""
+        req = self._signer.sign_request(request)
         res = self._session.send(req)
         response = json.loads(res.text)
         return response
 
+    def get_orders(self):
+        request = Request("GET", "%s/v1/orders" % self._config.base_url)
+        return self._send_request(request)
+
     def create_order(
         self,
-        symbol:str,
+        symbol: str,
         order_type: OrderType,
         order_quantity: float,
         side: Side,
     ):
-        req = self._signer.sign_request(
-            Request(
-                "POST",
-                "%s/v1/order" % self._config.base_url,
-                json={
-                    "symbol": symbol,
-                    "order_type": str(order_type),
-                    "order_quantity": order_quantity,
-                    "side": str(side),
-                },
-            )
+        # TODO: Need to add more to the JSON for limit orders
+        request = Request(
+            "POST",
+            "%s/v1/order" % self._config.base_url,
+            json={
+                "symbol": symbol,
+                "order_type": str(order_type),
+                "order_quantity": order_quantity,
+                "side": str(side),
+            },
         )
-        res = self._session.send(req)
-        response = json.loads(res.text)
-        return response
+        return self._send_request(request)
+
+    def cancel_all_orders(self):
+        request = Request(
+            "DELETE",
+            "%s/v1/orders"
+            % self._config.base_url,  # Be careful, orders has to be plural here
+        )
+        return self._send_request(request)
